77292a65e011ddb958ef45a521242c17c2c98bd5
尹吉欢 <yinjihuan@fangjia.com>
2019-04-22 15:55:51 +0800

    sql提交

---
+++ sjdbc-sharding-table-springboot/src/main/resources/loudong.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `loudong` (
+  `id` varchar(20) NOT NULL,
+  `city` varchar(20) NOT NULL,
+  `region` varchar(20) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `ld_num` varchar(10) NOT NULL,
+  `unit_num` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
